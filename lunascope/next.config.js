019e4b21912fd48f27c332e2c01d1a65a45eb119
henryshan30 <henry_shan@brown.edu>
2025-03-15 15:45:48 -0400

    Update next.config.js

--- lunascope/next.config.js
+++ lunascope/next.config.js
@@ -1,14 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "lunascope.org",
-        },
-      ],
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lunascope.org",
+      },
+      {
+        protocol: "https",
+        hostname: "bpb-us-w2.wpmucdn.com",
+      },
+      {
+        protocol: "https",
+        hostname: "amazingmagnets.com",
+      },
+      {
+        protocol: "https",
+        hostname: "www.meteorologiaenred.com",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
