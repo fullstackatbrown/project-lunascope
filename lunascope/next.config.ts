0ad4685affb980bce5e3cf2e86fabac03ca6d462
binary1011e <aidan_c_wang@brown.edu>
2025-04-12 16:23:41 -0400

    remove redirecting to team page

--- lunascope/next.config.ts
+++ lunascope/next.config.ts
@@ -1,15 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    async redirects() {
-        return [
-          {
-            source: '/',
-            destination: '/team',
-            permanent: false, // set to false if this might change in the future
-          },
-        ];
-    },
 };
 
 export default nextConfig;
--- lunascope/src/app/team/page.tsx
+++ lunascope/src/app/team/page.tsx
@@ -1,20 +1,21 @@
-import { teamData } from "../_components/teamdata";
-import Intro from "../_components/intro"
-import ProfileGroup from "../_components/group"
-import Navbar from "../_components/Navbar";
+import { teamData } from "./_components/teamdata";
+import Intro from "./_components/intro"
+import ProfileGroup from "./_components/group"
 
 export default function Home() {
     return (
         <div>
-            <Navbar />
             <div className="flex flex-col my-10 mx-10">
-            <Intro />
-            <div className="px-4 py-8">
-                {teamData.map((group, index) => (
-                <ProfileGroup key={index} sizing={group.large} title={group.role} members={group.members} />
-                ))}
+                <Intro />
+                <div className="px-4 py-8">
+                    {teamData.map((group, index) => (
+                    <ProfileGroup key={index} sizing={group.large} title={group.role} members={group.members} />
+                    ))}
+                </div>
             </div>
-        </div>
+            <footer className="bg-gray-500 w-full py-3 pl-12 mt-auto">
+                <p>&copy; 2025 My Website</p>
+            </footer>
         </div>
     );
 }
